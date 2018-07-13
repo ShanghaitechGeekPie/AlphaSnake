12e580a5f40ac3b62f7b4f75c297eb32d72a11b9
ZiwenZhuang <leo416850130@outlook.com>
2018-07-13 10:53:39 +0800

    updating Field main interface

--- AlphaSnakeCentrol/AlphaSnakeCentrol/field.py
+++ AlphaSnakeCentrol/AlphaSnakeCentrol/field.py
@@ -24,23 +24,17 @@ class Field():
     and return the whole map and the users' state after this iteration.
         Using 'enroll' method to add one or a list of participants into the field.
     '''
-    def __init__(self, map_size=(100, 100)):
+    def __init__(self, num_users, map_size=(100, 100)):
         '''
             Given information to setup the field to start the game.
         '''
-        # The map contains 100*100 bytes which represents the state of the pixel (occupied by user or food or unoccupied)
-        self.map = np.zeros(map_size, dtype=int)
+        # The map contains 100*100 bytes which represents the state of the pixel (occupied by user or food or
+        # unoccupied)
+        self.map = np.zeros(map_size, dtype=np.int8)
 
-        # A dictionary using user ID as key, a tuple as value which contains the state of the user and the body
-        # (a list of coordinates) of the snake. index 0 gets the head coordinate.
-        self.users = dict()
-
-    def enroll(self, users=[]):
-        '''
-            Given a list of participants (users) ID, this will return a number with the number of ID that existed
-        before (0 means no ID repeated)
-        '''
-        return 0
+        # A dictionary using user ID (a non-negative number) as index, a tuple as value which contains the state
+        # of the user and the body (a list of coordinates) of the snake. index 0 gets the head coordinate.
+        self.users = [] #TODO (initialize users info later in this function)
 
     def go(self, moves=None):
         '''
