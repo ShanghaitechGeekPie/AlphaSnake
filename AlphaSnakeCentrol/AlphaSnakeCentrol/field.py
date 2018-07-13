f85149dc498351d3e1afdd208ad8ac05606eecb4
ZiwenZhuang <leo416850130@outlook.com>
2018-07-13 10:39:11 +0800

    define main interface of the field judger

--- AlphaSnakeCentrol/AlphaSnakeCentrol/field.py
+++ AlphaSnakeCentrol/AlphaSnakeCentrol/field.py
@@ -1,3 +1,4 @@
+import numpy as np
 '''
     This part controls the update of the field. Defining field matrix as follows:
         x means the row index, y means the column index
@@ -24,7 +25,11 @@ class Field():
         Using 'enroll' method to add one or a list of participants into the field.
     '''
     def __init__(self, map_size=(100, 100)):
-        self.map = [[0 for j in range(map_size[1])] for i in range(map_size[0])]
+        '''
+            Given information to setup the field to start the game.
+        '''
+        # The map contains 100*100 bytes which represents the state of the pixel (occupied by user or food or unoccupied)
+        self.map = np.zeros(map_size, dtype=int)
 
         # A dictionary using user ID as key, a tuple as value which contains the state of the user and the body
         # (a list of coordinates) of the snake. index 0 gets the head coordinate.
@@ -40,7 +45,8 @@ class Field():
     def go(self, moves=None):
         '''
             Given the movement of each participant, return the whole map and a dictionary of participants' states.
-            moves: a dictionary with participants' ID as key and a number (0,1,2,3) as moving direction.
+            moves: a dictionary with participants' ID as key and a number (0,1,2,3) as value indicates the moving
+        direction.
             return: a matrix represented in a 2D-list (index using [i][j])
                 and a dictionary with participants' ID as key and a number representing state (0: dead, 1: alive)
         '''
