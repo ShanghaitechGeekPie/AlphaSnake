44b1cdade8e01d3cc53114dcb3e781ad2367f8d0
philipxyc <philipxyc@live.com>
2018-07-15 17:35:00 +0800

    change server name & msg type is list

--- AlphaSnakeCentrol/AlphaSnakeCentrol/WaitOnceEg.py
+++ AlphaSnakeCentrol/AlphaSnakeCentrol/WaitOnceEg.py
@@ -1,21 +1,24 @@
 # example of wait for one message
 
 import threading
+import copy
 from socketIO_client import SocketIO
 class WaitUntillRecv:
     def __init__(self):
         print('init')
+        self.msg = []
         self.received = False
-        self.socketio = SocketIO('http://ss.chinacloudsites.cn/', verify=False)
-        self.socketio.on('player0', self.on_event) # once received
+        self.socketio = SocketIO('https://as.chinacloudsites.cn/', verify=False)
+        self.socketio.on('judged', self.on_event) # once received
         self.receive_events_thread = threading.Thread(target=self._receive_events_thread) # new thread for listening forever
         self.receive_events_thread.daemon = True
         self.receive_events_thread.start()
-    def on_event(self, map):
-        self.received = True
-        self.map = map
+    def on_event(self, msg):
+        print(type(msg))
+        self.msg = copy.deepcopy(msg)
         self.socketio.disconnect() # stop waiting
         print('received!')
+        self.received = True
     def _receive_events_thread(self):
         self.socketio.wait()
         print('ended!')
@@ -26,4 +29,4 @@ waitClas = WaitUntillRecv()
 while not waitClas.received:
     pass
 
-print(waitClas.map)
+print(waitClas.msg)
